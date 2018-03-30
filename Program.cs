d7efeadf7600c0d9ec458e6e9d686222279e6180
metadojoai <juanblockchain.suero@gmail.com>
2018-03-30 13:23:53 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetcore2blockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
